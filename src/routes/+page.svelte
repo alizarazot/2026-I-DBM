6b47eb528f04b9b86349858df3743376cd353a23
alizarazot <alizarazot@ufpso.edu.co>
2026-03-18 03:28:55 -0500

    Add simple login demostration

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,12 @@
-<script lang="ts">
-</script>
+<script>
+	import { goto } from '$app/navigation';
+	import { userState } from '$lib/user.svelte.ts';
 
-Hello, homies! See the <a href="/record">Record</a>!
+	$effect((_) => {
+		if (userState.userType == null) {
+			goto('/auth');
+		} else {
+			goto(`/${userState.userType}`);
+		}
+	});
+</script>
