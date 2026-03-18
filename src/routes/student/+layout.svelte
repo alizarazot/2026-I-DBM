baac258bd4c367a3ec091c3a6d520d91d8910d55
alizarazot <alizarazot@ufpso.edu.co>
2026-03-18 02:19:21 -0500

    Add basic layout

---
+++ src/routes/student/+layout.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	let { children } = $props();
+
+	import { Navbar, NavBrand, ButtonGroup, Button, Avatar } from 'flowbite-svelte';
+	import { BellOutline } from 'flowbite-svelte-icons';
+</script>
+
+<Navbar>
+	<NavBrand>
+		<span>VoiceClass</span>
+	</NavBrand>
+
+	<ButtonGroup>
+		<Button href="/student/broadcast">Transmisión</Button>
+		<Button href="/student/tasks">Tareas</Button>
+		<Button href="/student/details">Detalles</Button>
+	</ButtonGroup>
+
+	<div class="flex items-center gap-2">
+		<BellOutline class="size-7" />
+		<Avatar class="size-7" />
+	</div>
+</Navbar>
+
+{@render children()}
