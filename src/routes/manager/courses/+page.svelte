72850e3b0b819454d348602294b141eff4ef3a10
alizarazot <alizarazot@ufpso.edu.co>
2026-03-23 20:37:17 -0500

    AI: Replicate

--- src/routes/manager/courses/+page.svelte
+++ src/routes/manager/courses/+page.svelte
@@ -1,1 +1,15 @@
-Cursos
+<script lang="ts">
+import { Button } from "flowbite-svelte";
+import CoursesTable from "./CoursesTable.svelte";
+import Register from "./Register.svelte";
+
+let showRegister = $state(false);
+</script>
+
+<header class="flex justify-end px-3">
+	<Button onclick={(_: any) => (showRegister = true)}>Registrar curso</Button>
+</header>
+
+<Register bind:open={showRegister} />
+
+<CoursesTable />
---
+++ src/routes/manager/courses/CoursesTable.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+import type { DataTableOptions } from "@flowbite-svelte-plugins/datatable";
+import { Table } from "@flowbite-svelte-plugins/datatable";
+
+import { courses } from "$lib/dummy-db.json";
+
+const dataTableOptions: DataTableOptions = {
+	data: {
+		headings: ["ID", "Nombre", "Descripción", "Docente", "Fecha creación"],
+		data: courses.map((course) => [
+			course.id,
+			course.title,
+			course.description,
+			course.teacherId,
+			new Date(course.createdAt).toLocaleDateString(),
+		]),
+	},
+	columns: [{ select: 0, hidden: true }],
+};
+</script>
+
+<Table {dataTableOptions} />
