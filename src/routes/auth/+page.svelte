2740887a8c6fbbd0ab3a6cfb1e3009fd578e0144
alizarazot <alizarazot@ufpso.edu.co>
2026-03-25 03:13:01 -0500

    Add login/logout

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -1,20 +1,16 @@
 <script lang="ts">
 import { enhance } from "$app/forms";
 import type { ActionData } from "./$types";
-import {
-	Button,
-	ButtonGroup,
-	Input,
-	Label,
-	RadioButton,
-} from "flowbite-svelte";
+import { Button, Input, Label, Heading, Span, Hr } from "flowbite-svelte";
 
 let { form } = $props<{ form: ActionData }>();
 </script>
 
 <div class="grid h-dvh w-dvw place-items-center">
-	<div>
-	<form method=post action="?/signInEmail" class="flex max-w-sm flex-col gap-3" use:enhance>
+	<div class="flex flex-col items-center">
+		<Heading class="text-center"> Plataforma <br> <Span highlight="orange">LINEA</Span> </Heading>
+			<br>
+	<form method=post action="?/signInEmail" class="flex w-full flex-col gap-3" use:enhance>
 		<div>
 			<Label>Correo electrónico</Label>
 			<Input type=email name=email />
