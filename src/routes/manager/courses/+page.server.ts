6cd82b44bfb00b8b518b2aa0b4a86aa720722cd9
alizarazot <alizarazot@ufpso.edu.co>
2026-03-25 04:41:09 -0500

    UD -> Courses

--- src/routes/manager/courses/+page.server.ts
+++ src/routes/manager/courses/+page.server.ts
@@ -1,9 +1,11 @@
 import { auth } from "$lib/server/auth";
 import { collectionCourses, type Course } from "$lib/server/database";
+import { ObjectId } from "mongodb";
 import type { PageData } from "../$types";
 import type { Actions, PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async (event) => {
+	event.depends("manager:courses");
 	let users: any;
 	try {
 		users = await auth.api.listUsers({
@@ -17,6 +19,7 @@ export const load: PageServerLoad = async (event) => {
 	const courses = [];
 	for await (const course of collectionCourses.find()) {
 		courses.push({
+			id: course._id.toString(),
 			name: course.name,
 			description: course.description,
 			maxStudents: course.maxStudents,
@@ -31,6 +34,34 @@ export const load: PageServerLoad = async (event) => {
 };
 
 export const actions: Actions = {
+	getCourse: async (event) => {
+		const formData = await event.request.formData();
+		const course = await collectionCourses.findOne({
+			_id: new ObjectId(formData.get("id")?.toString() ?? ""),
+		});
+		return { course: JSON.parse(JSON.stringify(course)) };
+	},
+	editCourse: async (event) => {
+		const formData = await event.request.formData();
+		const course: Course = {
+			name: formData.get("name")?.toString() ?? "",
+			description: formData.get("description")?.toString() ?? "",
+			maxStudents: parseInt(formData.get("maxStudents")?.toString() ?? "0"),
+			teacherId: formData.get("teacherId")?.toString() ?? "",
+		};
+		const id = formData.get("id")?.toString() ?? "";
+		console.log("id is", id);
+		await collectionCourses.updateOne(
+			{ _id: new ObjectId(id) },
+			{ $set: course },
+		);
+	},
+	deleteCourse: async (event) => {
+		const formData = await event.request.formData();
+		await collectionCourses.deleteOne({
+			_id: new ObjectId(formData.get("id")?.toString() ?? ""),
+		});
+	},
 	addCourse: async (event) => {
 		const formData = await event.request.formData();
 		const course: Course = {
--- src/routes/manager/courses/Register.svelte
+++ src/routes/manager/courses/Register.svelte
@@ -1,9 +1,65 @@
 <script lang="ts">
-import { enhance } from "$app/forms";
+import { deserialize, enhance } from "$app/forms";
 import { Button, Input, Label, Modal, Select } from "flowbite-svelte";
 
-let { openKind = $bindable(false), updateId = $bindable(""), users } = $props();
+let { openKind = $bindable(null), updateId = $bindable(""), users } = $props();
 
+let open = $state(false);
+
+$effect(() => {
+	open = openKind != null;
+});
+
+let action = $derived.by(() => {
+	switch (openKind) {
+		case null:
+			return "";
+		case "register":
+			return "/manager/courses?/addCourse";
+		case "update":
+			return "/manager/courses?/editCourse";
+	}
+});
+
+let actionLabel = $derived.by(() => {
+	switch (openKind) {
+		case null:
+			return "";
+		case "register":
+			return "Añadir";
+		case "update":
+			return "Editar";
+	}
+});
+
+let name = $state("");
+let description = $state("");
+let maxStudents = $state("");
+let teacherId = $state("");
+
+$effect(() => {
+	const id = updateId;
+	if (id === "") return;
+
+	(async function () {
+		const formData = new FormData();
+		formData.append("id", id);
+		const response = await fetch("/manager/courses?/getCourse", {
+			method: "POST",
+			body: formData,
+		});
+		const text = await response.text();
+		const result = deserialize(text);
+		if (result.type != "success") {
+			return;
+		}
+		const course = result.data!.course as any;
+		name = course.name;
+		description = course.description;
+		maxStudents = course.maxStudents;
+		teacherId = course.teacherId;
+	})();
+});
 const teachers = $derived.by(() =>
 	users
 		.filter((user) => user.role === "teacher")
@@ -12,35 +68,42 @@ const teachers = $derived.by(() =>
 			name: `${user.name} ${user.lastName}`,
 		})),
 );
+
+let errorMsg = $state();
 </script>
 
-<Modal open={openKind != null} size="xs">
-		<form method=POST action="/manager/courses?/addCourse" use:enhance class="flex flex-col space-y-6">
-		<h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Añadir curso</h3>
+<Modal open={open} onclose={()=>{openKind = null}} size="xs">
+		<form method=POST action={action} use:enhance class="flex flex-col space-y-6">
+		<h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">{actionLabel} curso</h3>
+
+		{#if openKind === "update"}
+		<Input name=id type=hidden bind:value={updateId}/>
+		{/if}
 
 		<Label class="space-y-2">
 			<span>Nombre</span>
-			<Input name=name maxlength={100} required />
+			<Input name=name maxlength={100} required bind:value={name} />
 		</Label>
 
 		<Label class="space-y-2">
 			<span>Descripción</span>
-			<Input name=description maxlength={200} required/>
+			<Input name=description maxlength={200} required bind:value={description}/>
 		</Label>
 
 		<Label class="space-y-2">
 			<span>Cantidad máxima de estudiantes</span>
-			<Input name=maxStudents type=number required />
+			<Input name=maxStudents type=number required bind:value={maxStudents}/>
 		</Label>
 		
 		<Label class="space-y-2">
 			<span>Profesor</span>
-			<Select name=teacherId items={teachers} placeholder="Seleccionar profesor..."/>
+			<Select name=teacherId items={teachers} placeholder="Seleccionar profesor..." bind:value={teacherId}/>
 		</Label>
 
 		<div class="flex justify-end gap-2">
-			<Button type=submit>Añadir</Button>
-			<Button onclick={close}>Cancelar</Button>
+			<Button type=submit>{actionLabel}</Button>
+			<Button onclick={()=> {openKind =null}}>Cancelar</Button>
 		</div>
+		<p class="text-red-500">{errorMsg}</p>
 	</form>
 </Modal>
