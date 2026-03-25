75d8345dccf749c3bae62ad6c45e476829ff6815
alizarazot <alizarazot@ufpso.edu.co>
2026-03-25 04:06:13 -0500

    CR -> Courses

--- src/lib/server/database.ts
+++ src/lib/server/database.ts
@@ -9,19 +9,11 @@ const client = new MongoClient(env.MONGODB_URI);
 
 export const db = client.db("Plataforma-LINEA");
 
-export type Class = {
-	transcription: [{ timestamp: number; text: string; personNumber: number }];
-	date: Date;
-	durationInSeconds: number;
-};
 export type Course = {
 	name: string;
 	description: string;
-	courseKind: string;
-	classIds: string[];
 	maxStudents: number;
 	teacherId: string;
-	calendar: { day: string; hourStart: string; hourEnd: string }[];
 };
 
 export const collectionCourses = db.collection("courses");
--- src/routes/manager/courses/+page.server.ts
+++ src/routes/manager/courses/+page.server.ts
@@ -1,20 +1,32 @@
+import { auth } from "$lib/server/auth";
 import { collectionCourses, type Course } from "$lib/server/database";
 import type { PageData } from "../$types";
 import type { Actions, PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async (event) => {
+	let users: any;
+	try {
+		users = await auth.api.listUsers({
+			query: {},
+			headers: event.request.headers,
+		});
+	} catch (err) {
+		console.log(err);
+	}
+
 	const courses = [];
 	for await (const course of collectionCourses.find()) {
 		courses.push({
 			name: course.name,
 			description: course.description,
-			courseKind: course.courseKind,
 			maxStudents: course.maxStudents,
+			teacherId: course.teacherId,
 		});
 	}
 
 	return {
 		courses: courses,
+		users: users.users,
 	};
 };
 
@@ -24,11 +36,8 @@ export const actions: Actions = {
 		const course: Course = {
 			name: formData.get("name")?.toString() ?? "",
 			description: formData.get("description")?.toString() ?? "",
-			courseKind: formData.get("courseKind")?.toString() ?? "",
 			maxStudents: parseInt(formData.get("maxStudents")?.toString() ?? "0"),
-			classIds: [],
-			teacherId: "",
-			calendar: [],
+			teacherId: formData.get("teacherId")?.toString() ?? "",
 		};
 		await collectionCourses.insertOne(course);
 	},
