78b18bc3dbb4329aa5a3dd7f5a09825b4e57c5e5
alizarazot <alizarazot@ufpso.edu.co>
2026-04-22 23:40:12 -0500

    Redo the audio system

---
+++ src/lib/uuid.ts
@@ -0,0 +1,6 @@
+// Taken from: <https://stackoverflow.com/a/2117523>.
+export const uuidV4AsString = () => {
+	return '10000000-1000-4000-8000-100000000000'.replace(/[018]/g, (c) =>
+		(+c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (+c / 4)))).toString(16)
+	);
+};
--- static/pcm-webaudio.js
+++ static/pcm-webaudio.js
@@ -1,24 +1,52 @@
+const SAMPLE_RATE = 16000;
 
-let pcmBuffer = new Float32Array();
+// Storing a full sample rate is equal to storing whole second of audio.
+const BUFFER_SECONDS = 1;
+const BUFFER_SIZE = SAMPLE_RATE * BUFFER_SECONDS;
 
 class PcmExtractor extends AudioWorkletProcessor {
-    process(inputs) {
-        const channel = inputs[0][0];
+	constructor() {
+		super();
 
-        try {
-            pcmBuffer = new Float32Array([...pcmBuffer, ...channel])
-        } catch (e) {
-            return true
-        }
+		this.pcmBuffer = new Float32Array(BUFFER_SIZE);
+		this.pcmBufferIdx = 0;
+	}
 
-        if (pcmBuffer.length >= 16000*2 ) {
-            console.info("Sending...", "Length:", pcmBuffer.length);
-            this.port.postMessage(pcmBuffer.buffer, [pcmBuffer.buffer])
-            pcmBuffer = new Float32Array();
-        }
+	process(inputs) {
+		if (inputs.length < 1) {
+			throw new Error('I need an input to process');
+		}
 
-        return true
-    }
+		// TODO: Check for the other input (if any).
+		if (inputs[0].length < 1) {
+			// TODO: Some times the browser doesn't expose the channels, although there are ones. Find a better way to do this.
+			console.error("The first input doesn't have a channel");
+			return true;
+		}
+
+		const pcm = inputs[0][0];
+
+		const remainingSpace = this.pcmBuffer.length - this.pcmBufferIdx;
+
+		if (pcm.length <= remainingSpace) {
+			this.pcmBuffer.set(pcm, this.pcmBufferIdx);
+			this.pcmBufferIdx += pcm.length;
+			return true;
+		}
+
+		// Fill rest of the array and send it.
+
+		this.pcmBuffer.set(pcm.subarray(0, remainingSpace), this.pcmBufferIdx);
+		this.port.postMessage(this.pcmBuffer.buffer, [this.pcmBuffer.buffer]);
+
+		this.pcmBuffer = new Float32Array(BUFFER_SIZE); // TODO: Investigate if this allocation could cause sample drops.
+
+		// TODO: `pcm` could still be larger than the buffer if the spec changes or the buffer is very small (<128 atm).
+		this.pcmBufferIdx = pcm.length - remainingSpace;
+		this.pcmBuffer.set(pcm.subarray(remainingSpace, pcm.length), 0);
+
+		return true;
+	}
 }
 
-registerProcessor("pcm-extractor", PcmExtractor)
+registerProcessor('pcm-extractor', PcmExtractor);
