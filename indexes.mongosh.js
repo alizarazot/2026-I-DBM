296f8025645bec90c409d133ca2229d876bdf3b3
alizarazot <alizarazot@ufpso.edu.co>
2026-04-27 17:17:17 -0500

    Update indexes

--- indexes.mongosh.js
+++ indexes.mongosh.js
@@ -1,4 +1,3 @@
-// 1. Conectar a la base de datos
 use('Plataforma-LINEA');
 
 db.users.createIndex({ email: 1 }, { unique: true });
@@ -6,7 +5,7 @@ db.users.createIndex({ email: 1 }, { unique: true });
 // Role is kinda 'sorter index', this case ascending.
 db.users.createIndex({ role: 1 });
 
-db.courses.createIndex({ teacherId: 1 });
+//db.courses.createIndex({ teacherId: 1 });
 
 // Token needs to be validated on each request.
 db.user_sessions.createIndex({ expiresAt: 1 });
