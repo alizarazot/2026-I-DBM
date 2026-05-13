56f3cb0f97043c987c2e43b0cfc49339fdb30715
alizarazot <alizarazot@ufpso.edu.co>
2026-05-13 06:59:33 -0500

    wip

--- src/routes/manager/students/+page.svelte
+++ src/routes/manager/students/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
-	import { Button } from 'flowbite-svelte';
+	import { Button, Modal } from 'flowbite-svelte';
+	import { Chart } from '@flowbite-svelte-plugins/chart';
+	import type { ApexOptions } from 'apexcharts';
 
 	import Register from '../Register.svelte';
 	import UsersTable from '../UsersTable.svelte';
@@ -16,16 +18,54 @@
 	async function deleteUser() {
 		const formData = new FormData();
 		formData.append('id', currentId);
-		const response = await fetch('/auth?/deleteUser', {
+		await fetch('/auth?/deleteUser', {
 			method: 'POST',
 			body: formData
 		});
 		invalidate('manager:users');
 		currentId = '';
 	}
+
+	let hours = [];
+	for (let i = 6; i <= 20; i++) {
+		hours.push(`${i}:00`);
+	}
+
+	let showAttendanceModal = $state(false);
+	let attendanceChartOptions: ApexOptions = {
+		chart: {
+			height: '400px',
+			toolbar: {
+				show: false
+			}
+		},
+		series: [
+			{
+				name: 'Asistencias (hombres)',
+				data: [1, 2, 3, 4]
+			},
+			{
+				name: 'Asistencias (mujeres)',
+				data: [5, 6, 7, 8],
+				color: '#f6339a'
+			}
+		],
+		xaxis: {
+			categories: hours
+		}
+	};
 </script>
 
+<Modal bind:open={showAttendanceModal}>
+	<Chart options={attendanceChartOptions} />
+</Modal>
+
 <header class="me-1 flex justify-end gap-2 px-3">
+	<Button
+		onclick={() => {
+			showAttendanceModal = true;
+		}}>Gráfica de asistencias</Button
+	>
 	<Button
 		onclick={() => {
 			registerOpenKind = 'register';
