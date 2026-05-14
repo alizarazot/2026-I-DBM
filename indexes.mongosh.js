2d36caedc35e04730f77da264cfed9b71a0113a4
alizarazot <alizarazot@ufpso.edu.co>
2026-05-14 04:10:54 -0500

    Last known good state

--- indexes.mongosh.js
+++ indexes.mongosh.js
@@ -7,7 +7,7 @@ db.user_sessions.createIndex({ expiresAt: 1 }, { expireAfterSeconds: 0 });
 db.user_sessions.createIndex({ token: 1 }, { unique: true });
 db.transcriptions.createIndex({ sessionId: 1 });
 
-// 2. Compound Index (e.g., search users by last name and first name)
+// 2. Compound Index
 db.users.createIndex({ lastName: 1, firstName: 1 });
 
 // 3. Multikey Index (indexing the studentsIds array in courses)
@@ -16,5 +16,5 @@ db.courses.createIndex({ studentsIds: 1 });
 // 4. Text Index (searching course names and descriptions)
 db.courses.createIndex({ name: 'text', description: 'text' });
 
-// 5. Geospatial Index (for student location analytics)
+// 5. Geospatial Index
 db.user_location_analytics.createIndex({ coordinates: '2dsphere' });
